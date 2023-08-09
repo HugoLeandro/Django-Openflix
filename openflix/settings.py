52384f28bbc8160b40d77c9203e0a238bf83f371
siteflask1 <leandrolimahugo@gmail.com>
2023-08-09 09:23:00 -0300

    update commit

--- openflix/settings.py
+++ openflix/settings.py
@@ -21,17 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# TOKEN_CSRF = os.getenv('TOKEN_CSRF')
-# if TOKEN_CSRF:
-#     SECRET_KEY = TOKEN_CSRF
-#     CSRF_TRUSTED_ORIGINS = ['https://django-openflix-production.up.railway.app']
-# else:
-SECRET_KEY = "!#@#!@#!@#!#!#QWEQWEQWEQEQWE"#os.getenv('SECRET_KEY')
-
+TOKEN_CSRF = os.getenv('TOKEN_CSRF')
+if TOKEN_CSRF:
+    SECRET_KEY = TOKEN_CSRF
+    CSRF_TRUSTED_ORIGINS = ['https://django-openflix-production.up.railway.app']
+else:
+    SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['django-openflix-production.up.railway.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
